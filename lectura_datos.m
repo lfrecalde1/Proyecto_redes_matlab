10f40cfa79bf54fc1d33a0c22ca58ae97b6191f8
lfrecalde1 <lfrecalde1@espe.edu.ec>
2021-01-25 10:38:33 -0500

    base datos

--- lectura_datos.m
+++ lectura_datos.m
@@ -6,3 +6,9 @@ clc,clear all,close all;
 
 %% Lectura de los datos de la base 
 consumo_poste=Consumo_poste(14);
+%% Definir poste a buscar
+poste_1=10879;
+poste_2=194409;
+poste_3=10888;
+postes=[poste_1,]
+Final=consumo_poste.Consumo(consumo_poste.Poste==poste_1)
