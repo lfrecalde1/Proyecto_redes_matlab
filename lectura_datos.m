cd58ab6d1ddfc6abe7e47ba288eed25d7785fb6e
lfrecalde1 <lfrecalde1@espe.edu.ec>
2021-01-26 07:38:12 -0500

    verificacion

--- lectura_datos.m
+++ lectura_datos.m
@@ -1,6 +1,5 @@
 %% Programa de transcripcion de lo que se encuentra en excel
 
-
 %% Eliminar todas la variables del sistema
 clc,clear all,close all;
 
@@ -11,6 +10,8 @@ kva_m = kvam();
 poste_1=10879;
 poste_2=194409;
 poste_3=10888;
+
+
 
 %% DEFINIR EL VECTOR DE LOS POSTES
 postes=[poste_1,poste_2,poste_3];
@@ -24,7 +25,7 @@ long=[long_1,long_2,long_3];
 
 %% Definir el tipo de conductor a usar
 conductor_1="PRE. AL2X50(50)";
-conductor_2="PRE. AL2X35(35)";
+conductor_2="PRE. AL2X50(50)";
 conductor_3="PRE. AL2X50(50)";
 conductor=[conductor_1,conductor_2,conductor_3];
 
@@ -42,3 +43,9 @@ tipo_conexion="monofasica";
 
 %% GERNERACION DE LA MATRIX DE LOS DATOS INGRESADOS
 [Datos] = generacion_datos(postes,long,consumo,aux_c,conexion,aux_t);
+
+[i_dato,j_dato]=size(Datos)
+for k=1:j_dato
+    Parcial(k)=(Datos(2,k)*sum(Datos(3,1:k)))/Datos(4,k);
+end
+rsultado_1=sum(Parcial)
